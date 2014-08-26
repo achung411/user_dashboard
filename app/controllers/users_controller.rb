d3f137a29afcdca05f49a0281d68301293fc5a0f
Aaron Chung <a_h_chung@yahoo.com>
2014-08-25 20:14:31 -0700

    added description column, added uniqueness index to user model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,11 +20,11 @@ class UsersController < ApplicationController
 	end
 
 	def show
-		# @user = User.find(params)
+		@user = User.find(session[:user_id])
 	end
 
 	private
 		def user_params
-			params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+			params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :description)
 		end
 end
