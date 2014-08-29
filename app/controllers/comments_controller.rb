e413b3ad958145e45fffe343545341f15c09eda7
Aaron Chung <a_h_chung@yahoo.com>
2014-08-28 17:46:40 -0700

    users can add comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,19 @@
 class CommentsController < ApplicationController
-	
   def create
-  	render text: params
+  	@comment = Comment.new(user: User.find(comment_params[:user_id]), content: comment_params[:content], message: Message.find(comment_params[:message_id]), author: User.find(session[:user_id]))
+  	if @comment.save
+  		flash[:notice] = "Response was successfully posted."
+  	else
+  		flash[:errors] = @comment.errors.full_messages
+  	end
+  	redirect_to :back
   end
 
   def destroy
+  end
+
+  private
+  def comment_params
+  	params.require(:comment).permit(:user_id, :message_id, :content)
   end
 end
